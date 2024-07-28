5463b0ef2497521f0635ad45252ddb5185df8c3b
AnikHadi <anikhadi9@gmail.com>
2024-07-28 12:38:41 +0600

    complete enumData type

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-var userName = "Hadi";
+var RequestOptions;
+(function (RequestOptions) {
+    RequestOptions["get"] = "GET";
+    RequestOptions["post"] = "POST";
+    RequestOptions["put"] = "PUT";
+    RequestOptions["patch"] = "PATCH";
+    RequestOptions["head"] = "HEAD";
+    RequestOptions["options"] = "OPTIONS";
+    RequestOptions["trace"] = "TRACE";
+    RequestOptions["connect"] = "CONNECT";
+    RequestOptions["delete"] = "DELETE";
+})(RequestOptions || (RequestOptions = {}));
+console.log(RequestOptions.get); // "GET"
+console.log(RequestOptions["post"]); // "POST"
+var request = RequestOptions.get;
+console.log(request);
+var RequestStatus;
+(function (RequestStatus) {
+    RequestStatus[RequestStatus["OK"] = 200] = "OK";
+    RequestStatus[RequestStatus["CREATED"] = 201] = "CREATED";
+    RequestStatus[RequestStatus["ACCEPTED"] = 202] = "ACCEPTED";
+    RequestStatus[RequestStatus["NO_CONTENT"] = 204] = "NO_CONTENT";
+    RequestStatus[RequestStatus["MOVED_PERMANENTLY"] = 301] = "MOVED_PERMANENTLY";
+    RequestStatus[RequestStatus["FOUND"] = 302] = "FOUND";
+    RequestStatus[RequestStatus["BAD_REQUEST"] = 400] = "BAD_REQUEST";
+    RequestStatus[RequestStatus["UNAUTHORIZED"] = 401] = "UNAUTHORIZED";
+    RequestStatus[RequestStatus["FORBIDDEN"] = 403] = "FORBIDDEN";
+    RequestStatus[RequestStatus["NOT_FOUND"] = 404] = "NOT_FOUND";
+    RequestStatus[RequestStatus["CONFLICT"] = 409] = "CONFLICT";
+    RequestStatus[RequestStatus["SERVER_ERROR"] = 500] = "SERVER_ERROR";
+    RequestStatus[RequestStatus["NOT_IMPLEMENTED"] = 501] = "NOT_IMPLEMENTED";
+    RequestStatus[RequestStatus["BAD_GATEWAY"] = 502] = "BAD_GATEWAY";
+    RequestStatus[RequestStatus["SERVICE_UNAVAILABLE"] = 503] = "SERVICE_UNAVAILABLE";
+    RequestStatus[RequestStatus["GATEWAY_TIMEOUT"] = 504] = "GATEWAY_TIMEOUT";
+    RequestStatus[RequestStatus["HTTP_VERSION_NOT_SUPPORTED"] = 505] = "HTTP_VERSION_NOT_SUPPORTED";
+})(RequestStatus || (RequestStatus = {}));
+console.log(RequestStatus.OK);
+console.log(RequestStatus["ACCEPTED"]);
+var RequestType;
+(function (RequestType) {
+    RequestType["readData"] = "READ_DATA";
+    RequestType["writeData"] = "WRITE_DATA";
+    RequestType["updateData"] = "UPDATE_DATA";
+    RequestType["deleteData"] = "DELETE_DATA";
+    RequestType[RequestType["statusCode"] = 500] = "statusCode";
+    RequestType["errorMsg"] = "Internal Server Error";
+})(RequestType || (RequestType = {}));
+console.log(RequestType.writeData);
+console.log(RequestType["errorMsg"]);
--- index.ts
+++ index.ts
@@ -1,1 +1,57 @@
-let userName: string | number | boolean = "Hadi";
+enum RequestOptions {
+  get = "GET",
+  post = "POST",
+  put = "PUT",
+  patch = "PATCH",
+  head = "HEAD",
+  options = "OPTIONS",
+  trace = "TRACE",
+  connect = "CONNECT",
+  delete = "DELETE",
+}
+
+console.log(RequestOptions.get); // "GET"
+console.log(RequestOptions["post"]); // "POST"
+
+const request: RequestOptions = RequestOptions.get;
+console.log(request);
+
+enum RequestStatus {
+  OK = 200,
+  CREATED = 201,
+  ACCEPTED = 202,
+  NO_CONTENT = 204,
+  MOVED_PERMANENTLY = 301,
+  FOUND = 302,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  FORBIDDEN = 403,
+  NOT_FOUND = 404,
+  CONFLICT = 409,
+  SERVER_ERROR = 500,
+  NOT_IMPLEMENTED = 501,
+  BAD_GATEWAY = 502,
+  SERVICE_UNAVAILABLE = 503,
+  GATEWAY_TIMEOUT = 504,
+  HTTP_VERSION_NOT_SUPPORTED = 505,
+}
+
+console.log(RequestStatus.OK);
+console.log(RequestStatus["ACCEPTED"]);
+
+interface ApiResponse<T> {
+  status: RequestStatus;
+  data: T;
+}
+
+enum RequestType {
+  readData = "READ_DATA",
+  writeData = "WRITE_DATA",
+  updateData = "UPDATE_DATA",
+  deleteData = "DELETE_DATA",
+  statusCode = 500,
+  errorMsg = "Internal Server Error",
+}
+
+console.log(RequestType.writeData);
+console.log(RequestType["errorMsg"]);
