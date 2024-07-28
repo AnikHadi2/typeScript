fb5a62fdcf8d6be3b284167f34167d9a634b39ae
AnikHadi <anikhadi9@gmail.com>
2024-07-28 12:46:42 +0600

    complete unionData type

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-var userName = "Hadi";
+var userId;
+userId = 123;
+console.log(typeof userId);
+userId = "abc";
+console.log(typeof userId);
+userId = false;
+console.log(typeof userId);
+// function displayUser(userId: number | string | boolean) {
+function displayUser(userId) {
+    if (typeof userId === "number") {
+        console.log("User ID: ".concat(userId));
+    }
+    else if (typeof userId === "string") {
+        console.log("User Name: ".concat(userId));
+    }
+    else if (typeof userId === "boolean") {
+        console.log("User Status: ".concat(userId ? "Active" : "Inactive"));
+    }
+    else {
+        console.log("Invalid user ID");
+    }
+}
+displayUser(123);
+displayUser("John Doe");
+displayUser(true);
--- index.ts
+++ index.ts
@@ -1,1 +1,29 @@
-let userName: string | number | boolean = "Hadi";
+let userId: number | string | boolean;
+
+userId = 123;
+console.log(typeof userId);
+
+userId = "abc";
+console.log(typeof userId);
+
+userId = false;
+console.log(typeof userId);
+
+type userIdType = number | string | boolean;
+
+// function displayUser(userId: number | string | boolean) {
+function displayUser(userId: userIdType) {
+  if (typeof userId === "number") {
+    console.log(`User ID: ${userId}`);
+  } else if (typeof userId === "string") {
+    console.log(`User Name: ${userId}`);
+  } else if (typeof userId === "boolean") {
+    console.log(`User Status: ${userId ? "Active" : "Inactive"}`);
+  } else {
+    console.log("Invalid user ID");
+  }
+}
+
+displayUser(123);
+displayUser("John Doe");
+displayUser(true);
