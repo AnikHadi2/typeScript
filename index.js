94a3de21958f90f06da222d71a45f928cd0d95bc
AnikHadi <anikhadi9@gmail.com>
2024-07-28 13:02:08 +0600

    complete objectData type

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-var userName = "Hadi";
+// let user: object;
+var user;
+user = {
+    userId: "1",
+    userName: "Alice",
+    email: "alice@example.com",
+    password: "password123",
+};
+console.log(user);
+var user1;
+user1 = {
+    userId: "1",
+    email: "alice@example.com",
+    password: "password123",
+};
+console.log(user1);
+var userDemo;
+var users;
+users = [
+    {
+        userId: "1",
+        userName: "Alice",
+        email: "alice@example.com",
+        password: "password123",
+    },
+    {
+        userId: "2",
+        userName: "Bob",
+        email: "bob@example.com",
+        password: "password456",
+    },
+    {
+        userId: "3",
+        email: "charlie@example.com",
+        password: "password789",
+    },
+];
+console.log(users);
--- index.ts
+++ index.ts
@@ -1,1 +1,56 @@
-let userName: string | number | boolean = "Hadi";
+// let user: object;
+let user: { userId: string; userName: string; email: string; password: string };
+
+user = {
+  userId: "1",
+  userName: "Alice",
+  email: "alice@example.com",
+  password: "password123",
+};
+
+console.log(user);
+
+let user1: {
+  userId: string;
+  userName?: string;
+  email: string;
+  password: string;
+};
+
+user1 = {
+  userId: "1",
+  email: "alice@example.com",
+  password: "password123",
+};
+
+console.log(user1);
+
+let userDemo: {
+  userId: string;
+  userName?: string;
+  email: string;
+  password: string;
+};
+
+let users: Array<typeof userDemo>;
+users = [
+  {
+    userId: "1",
+    userName: "Alice",
+    email: "alice@example.com",
+    password: "password123",
+  },
+  {
+    userId: "2",
+    userName: "Bob",
+    email: "bob@example.com",
+    password: "password456",
+  },
+  {
+    userId: "3",
+    email: "charlie@example.com",
+    password: "password789",
+  },
+];
+
+console.log(users);
