8771d6f06e8ad68fa2e09388a4129497014a3e4b
AnikHadi <anikhadi9@gmail.com>
2024-07-31 11:22:55 +0600

    update

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-var userName = "Hadi";
+// function Signature
+// let userInfo1: () => void;
+var userInfo1 = function () {
+    console.log("My name is Hadiuzzaman Anik & age 27 years old.");
+};
+userInfo1();
+// let userInfo2: (name: string, age: number ) => void;
+var userInfo2 = function (name, age) {
+    console.log("My name is ".concat(name, " & age ").concat(age, " years old."));
+};
+userInfo2("Hadiuzzaman Anik", 27);
+// let userInfo3: (name: string, age: number ) => string;
+var userInfo3 = function (name, age) {
+    return "My name is ".concat(name, " & age ").concat(age, " years old.");
+};
+console.log(userInfo3("Hadiuzzaman Anik", 27));
--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
-let userName: string | number | boolean = "Hadi";
+// function Signature
+
+// let userInfo1: () => void;
+const userInfo1 = (): void => {
+  console.log(`My name is Hadiuzzaman Anik & age 27 years old.`);
+};
+userInfo1();
+
+// let userInfo2: (name: string, age: number ) => void;
+const userInfo2 = (name: string, age: number): void => {
+  console.log(`My name is ${name} & age ${age} years old.`);
+};
+userInfo2("Hadiuzzaman Anik", 27);
+
+// let userInfo3: (name: string, age: number ) => string;
+const userInfo3 = (name: string, age: number): string => {
+  return `My name is ${name} & age ${age} years old.`;
+};
+console.log(userInfo3("Hadiuzzaman Anik", 27));
