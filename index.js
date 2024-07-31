f93777828afc5d528ad1f14799881879e56e1175
AnikHadi <anikhadi9@gmail.com>
2024-07-31 12:21:27 +0600

    update interface as types

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-var userName = "Hadi";
+var users = [];
+var user1 = {
+    id: 1,
+    name: "John Doe",
+    age: 30,
+};
+var user2 = {
+    id: 2,
+    name: "Jane Smith",
+    age: 25,
+};
+var displayUserInfo = function (user) {
+    console.log("User ID: ".concat(user.id, ", Name: ").concat(user.name, ", Age: ").concat(user.age));
+};
+users.push(user1);
+users.push(user2);
+for (var _i = 0, users_1 = users; _i < users_1.length; _i++) {
+    var user = users_1[_i];
+    displayUserInfo(user);
+}
--- index.ts
+++ index.ts
@@ -1,1 +1,30 @@
-let userName: string | number | boolean = "Hadi";
+interface IUser {
+  id: number;
+  name: string;
+  age: number;
+}
+
+const users: IUser[] = [];
+
+const user1: IUser = {
+  id: 1,
+  name: "John Doe",
+  age: 30,
+};
+
+const user2: IUser = {
+  id: 2,
+  name: "Jane Smith",
+  age: 25,
+};
+
+const displayUserInfo = (user: IUser): void => {
+  console.log(`User ID: ${user.id}, Name: ${user.name}, Age: ${user.age}`);
+};
+
+users.push(user1);
+users.push(user2);
+
+for (let user of users) {
+  displayUserInfo(user);
+}
